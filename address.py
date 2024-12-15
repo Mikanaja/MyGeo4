57ba420f0c2ad040173b70d1531396bf94746fc5
Mikhael Machado Fernandes Maia <65919190+Mikanaja@users.noreply.github.com>
2024-12-15 19:59:25 -0300

    Delete address.py

--- address.py
+++
@@ -1,9 +0,0 @@
-# address.py
-
-# Lista de tuplas (IP, PORTA) dos datanodes
-ENDERECOS_DATANODES = [
-    ('localhost', 8001),  # Primeiro datanode
-    ('localhost', 8002),  # Segundo datanode
-    ('localhost', 8003),  # Terceiro datanode
-    ('localhost', 8004)   # Quarto datanode
-]
